86226bac27bf1e8390120737a1fee237140e6e6e
The Man <ThisIsntTheWay@users.noreply.github.com>
2022-11-03 20:24:46 +0100

    Update variables.tf

--- runbooks/variables.tf
+++ runbooks/variables.tf
@@ -1,6 +1,33 @@
+# ===================================================
+# Virtual Machines
+variable "serverType" {
+  description = "What type of service the VM will provide (ap, dc, db...)"
+}
+
+variable "serverSize" {
+  description = "Size of the VM, as defined here:\nhttps://azureprice.net/?region=switzerlandnorth"
+  default     = "Standard_A2_v2"
+}
+
+variable "serverSku" {
+  description = "SKU of the VM, as defined under 'Image SKU':\nhttps://learn.microsoft.com/en-us/azure/backup/backup-azure-policy-supported-skus#supported-vms"
+  default     = "2022-datacenter-azure-edition-smalldisk"
+}
+
+variable "adminUsername" {
+  description = "Username for the administrator account"
+}
+variable "adminPassword" {
+  description = "Password for the administrator account"
+}
+
+# ===================================================
+# General
 variable "resourceLocation" {
   default = "switzerlandnorth"
 }
 
 variable "tenant" {}
 variable "author" {}
+
+# ===================================================
