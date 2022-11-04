2fb860a60349ad9b9d7733d0306e3c268ac2c251
The Man <ThisIsntTheWay@users.noreply.github.com>
2022-11-04 23:40:01 +0100

    Backend data

--- runbooks/main.tf
+++ runbooks/main.tf
@@ -7,6 +7,15 @@ terraform {
     }
   }
 
+  backend "azurerm" {
+    # The following should be provided with -backend-config
+    #resource_group_name  = var.storageAccountRg
+    #storage_account_name = var.storageAccountName
+    #container_name       = var.storageAccountContainer
+
+    key                  = "terraform.tfstate"
+  }
+
   required_version = ">= 1.1.0"
 }
 
